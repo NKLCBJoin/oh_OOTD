fc1babf2d571dc364ef3d0d71649acb351b3a4e0
신근재 <shin75492@naver.com>
2022-10-22 11:47:03 +0900

    폴더명 변경 & 작업자 기록
    221022

--- ootd/lib/main.dart
+++ ootd/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:ootd/temp.dart';
 
+//신근재
+
 void main() {
   KakaoSdk.init(nativeAppKey: '5f71064329b935428862eb575059fe75');
   runApp(const MyApp());
@@ -37,77 +39,82 @@ class MyApp extends StatelessWidget {
     );
 
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Container(
             color: Colors.white,
             child: Center(
                 child: Column(
                   children: [
-                    SizedBox(height: 200,width: 400,),
+                    SizedBox(height: 200,width: 500,),
 
-                    Image.asset("images/mainpage_image.png", height: 300,width: 4300,),
+                    Image.asset("assets/mainpage_image.png", height: 300,width: 300,),
 
                     SizedBox(
-                          child: ElevatedButton.icon(
-                              icon: Icon(Icons.lock),
-                              label: Text("카카오 계정으로 로그인",style: TextStyle(fontSize: 17, color: Colors.black87),),
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(Colors.yellow),
-                                foregroundColor: MaterialStateProperty.all(Colors.black54),
-                              ),
+                      width: 500,
+                          child: Column(
+                            children: [
+                              ElevatedButton.icon(
+                                  icon: Icon(Icons.lock),
+                                  label: Text("카카오 계정으로 로그인",style: TextStyle(fontSize: 17, color: Colors.black87),),
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                                    foregroundColor: MaterialStateProperty.all(Colors.black54),
+                                  ),
 
-                              onPressed: () async {
-                                if (await isKakaoTalkInstalled()) {
-                                  try {
-                                    await UserApi.instance.loginWithKakaoTalk();
-                                    print('카카오톡으로 로그인 성공');
-                                    // Navigator.push(
-                                    //     context,
-                                    //     MaterialPageRoute(builder: (context) => const secondpage()));
-                                    _get_user_info();
+                                  onPressed: () async {
+                                    if (await isKakaoTalkInstalled()) {
+                                      try {
+                                        await UserApi.instance.loginWithKakaoTalk();
+                                        print('카카오톡으로 로그인 성공');
+                                        // Navigator.push(
+                                        //     context,
+                                        //     MaterialPageRoute(builder: (context) => const secondpage()));
+                                        _get_user_info();
 
-                                  } catch (error) {
-                                    print('카카오톡으로 로그인 실패 $error');
+                                      } catch (error) {
+                                        print('카카오톡으로 로그인 실패 $error');
+                                      }
+                                    }
                                   }
-                                }
-                              }
+                              ),
+                              ElevatedButton.icon(
+                                icon: Icon(Icons.autorenew_outlined),
+                                label: Text("로그아웃",style: TextStyle(fontSize: 17, color: Colors.black87),),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                                  foregroundColor: MaterialStateProperty.all(Colors.black54),
+                                ),
+                                onPressed: () async {
+                                  try {
+                                    await UserApi.instance.unlink();
+                                    print('연결 끊기 성공, SDK에서 토큰 삭제');
+                                  } catch (error) {
+                                    print('연결 끊기 실패 $error');
+                                  }
+                                },
+                              ),
+                              ElevatedButton.icon(
+                                icon: Icon(Icons.share_sharp),
+                                label: Text("공유하기",style: TextStyle(fontSize: 17, color: Colors.black87),),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(Colors.yellow),
+                                  foregroundColor: MaterialStateProperty.all(Colors.black54),
+                                ),
+                                onPressed: () async {
+                                  if(await ShareClient.instance.isKakaoTalkSharingAvailable()) {
+                                    try {
+                                      Uri uri = await ShareClient.instance.shareDefault(template: defaultFeed);
+                                      await ShareClient.instance.launchKakaoTalk(uri);
+                                      print('카카오톡 공유 완료');
+                                    } catch (error) {
+                                      print('카카오톡 공유 실패 $error');
+                                    }
+                                  }
+                                },
+                              ),
+                            ],
                           ),
                         ),
-                ElevatedButton.icon(
-                  icon: Icon(Icons.autorenew_outlined),
-                  label: Text("로그아웃",style: TextStyle(fontSize: 17, color: Colors.black87),),
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.yellow),
-                    foregroundColor: MaterialStateProperty.all(Colors.black54),
-                  ),
-                  onPressed: () async {
-                    try {
-                      await UserApi.instance.unlink();
-                      print('연결 끊기 성공, SDK에서 토큰 삭제');
-                    } catch (error) {
-                      print('연결 끊기 실패 $error');
-                    }
-                  },
-                ),
-
-                    ElevatedButton.icon(
-                      icon: Icon(Icons.autorenew_outlined),
-                      label: Text("공유하기",style: TextStyle(fontSize: 17, color: Colors.black87),),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.yellow),
-                        foregroundColor: MaterialStateProperty.all(Colors.black54),
-                      ),
-                      onPressed: () async {
-                        if(await ShareClient.instance.isKakaoTalkSharingAvailable()) {
-                          try {
-                            Uri uri = await ShareClient.instance.shareDefault(template: defaultFeed);
-                            await ShareClient.instance.launchKakaoTalk(uri);
-                            print('카카오톡 공유 완료');
-                          } catch (error) {
-                            print('카카오톡 공유 실패 $error');
-                          }
-                        }
-                      },
-                    ),
                   ],
                 )
             )
