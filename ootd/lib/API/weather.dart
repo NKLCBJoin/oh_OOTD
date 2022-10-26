ee96ed7b778c94d4ae859e37b2c6682e4a62a944
afg9327 <93969640+afg9327@users.noreply.github.com>
2022-10-26 10:12:48 +0900

    날씨.다트 파일 수정

--- ootd/lib/API/weather.dart
+++ ootd/lib/API/weather.dart
@@ -1,35 +1,14 @@
 import 'package:http/http.dart' as http;
 
-/*class Network{
-  final String url;
+class Network{
+
+  String url = 'https://api.openweathermap.org/data/2.5/weather?q=Gumi&appid=4293fbff2c2e4d5c80ce32cea6e1b5be';
   Network(this.url);
-  Fuuture<dynamic> getJson() async{
+  Future<dynamic> getJson() async{
     http.Response response =
         await http.get(Uri.parse(url));
 
     return response;
   }
-}*/
-
-class Loading extends StatefulWidget{
-  @override
-  _LoadingState createState() => _LoadingState();
 }
 
-
-class _LoadingState extends State<> {
-  @override
-  void initState(){
-    super.initState();
-    fetchData();
-  }
-  void fetchData() async{
-    Response response = await get('https://samples.openweathermap.org'
-        '/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1');
-    print(response.statusCode);
-  }
-
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
