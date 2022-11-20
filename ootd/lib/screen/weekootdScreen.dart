095bff89b5e2233eea3b1815634fe34f19595b78
최지철 <jife98@naver.com>
2022-11-20 20:21:58 +0900

    주간ootd ui 구성 작업완료

--- ootd/lib/screen/weekootdScreen.dart
+++ ootd/lib/screen/weekootdScreen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ootd/screen/loading.dart';
+import 'package:ootd/screen/mainScreen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:ootd/screen/menu.dart';
 //최지철
 class WeekootdPage extends StatefulWidget {
   const WeekootdPage({Key? key}) : super(key: key);
-
   @override
   _WeekootdPageState createState() => _WeekootdPageState();
 }
@@ -14,42 +18,59 @@ class _WeekootdPageState extends State<WeekootdPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       key: scaffoldKey,
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.deepPurpleAccent,
-        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         leading: IconButton(
           icon: Icon(
             Icons.arrow_back,
             size: 30,
           ),
           onPressed: () {
-            print('IconButton pressed ...');
+            Navigator.push(context, MaterialPageRoute(builder: (_)=>Loading()));
           },
         ),
-        actions: [],
+        actions:<Widget> [
+          IconButton(
+            icon: Icon(
+              Icons.menu,
+              size: 30,
+            ),
+            onPressed: ()  {
+            },
+          ),
+
+        ],
         centerTitle: false,
-        elevation: 2,
       ),
-      body: SafeArea(
+
+      body: Center(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
           child: Container(
             width: double.infinity,
             height: double.infinity,
             decoration: BoxDecoration(
-              color: Colors.white,
+                gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color(0xffa1c4fd),
+                      Color(0xffc2e9fb),
+                    ]
+                )
             ),
             child: ListView(
-              padding: EdgeInsets.zero,
+              padding: EdgeInsetsDirectional.fromSTEB(10, 65, 10, 10),
               scrollDirection: Axis.vertical,
               children: [
                 Container(
                   width: double.infinity,
-                  height: 400,
+                  height: 600,
                   decoration: BoxDecoration(
-                    color: Colors.white,
+                    color: Colors.transparent,
                   ),
                   child: ListView(
                     padding: EdgeInsets.zero,
@@ -58,17 +79,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -76,6 +97,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Monday',
+                              style: GoogleFonts.kanit(
+                                textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                fontSize: 30,
+                                fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -83,17 +109,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -101,6 +127,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Tuesday',
+                              style: GoogleFonts.kanit(
+                                  textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -108,17 +139,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -126,6 +157,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Wednesday',
+                              style: GoogleFonts.kanit(
+                                  textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -133,17 +169,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -151,6 +187,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Thursday',
+                              style: GoogleFonts.kanit(
+                                  textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -158,17 +199,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -176,6 +217,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Friday',
+                              style: GoogleFonts.kanit(
+                                  textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -183,17 +229,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -201,6 +247,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Saturday',
+                              style: GoogleFonts.kanit(
+                                  textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -208,17 +259,17 @@ class _WeekootdPageState extends State<WeekootdPage> {
                       Padding(
                         padding: EdgeInsetsDirectional.fromSTEB(10, 10, 10, 10),
                         child: Container(
-                          width: 300,
+                          width: 380,
                           height: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
+                            color: Colors.white,
                             borderRadius: BorderRadius.circular(35),
                             boxShadow: [
                               BoxShadow(
-                                color: Colors.grey.withOpacity(0.7),
+                                color: const Color(0xffa1c4fd).withOpacity(0.7),
                                 spreadRadius: 0,
                                 blurRadius: 5.0,
-                                offset: Offset(0, 10), // changes position of shadow
+                                offset: Offset(4, 6), // changes position of shadow
                               ),
                             ],
                           ),
@@ -226,6 +277,11 @@ class _WeekootdPageState extends State<WeekootdPage> {
                             alignment: AlignmentDirectional(0, -1),
                             child: Text(
                               'Sunday',
+                              style: GoogleFonts.kanit(
+                                  textStyle: TextStyle(color: Colors.black,letterSpacing: 5),
+                                  fontSize: 30,
+                                  fontStyle: FontStyle.normal
+                              ),
                             ),
                           ),
                         ),
@@ -233,93 +289,6 @@ class _WeekootdPageState extends State<WeekootdPage> {
                     ],
                   ),
                 ),
-            Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(10, 30, 10, 10),
-                child:Container(
-                  width: double.infinity,
-                  height: 500,
-                  decoration: BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.circular(35),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.7),
-                        spreadRadius: 0,
-                        blurRadius: 5.0,
-                        offset: Offset(0, 10), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    scrollDirection: Axis.vertical,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(0),
-                            bottomRight: Radius.circular(0),
-                            topLeft: Radius.circular(35),
-                            topRight: Radius.circular(35),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                        ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.pink,
-                        ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                        ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                        ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(35),
-                            bottomRight: Radius.circular(35),
-                            topLeft: Radius.circular(0),
-                            topRight: Radius.circular(0),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                ),
-            ),
               ],
             ),
           ),
