50cc9ff27009d60bf9665fc8e1a8b472f9e70802
신근재 <shin75492@naver.com>
2022-11-25 01:58:02 +0900

    카톡 미설치 시 알림 추가
    221124 카톡 미설치 시에 로그인 버튼을 누르면 AlertDialog를 통해 알림창을 뜨게 해준다.

--- ootd/lib/API/kakao.dart
+++ ootd/lib/API/kakao.dart
@@ -5,27 +5,78 @@ import '../API/gsheets.dart';
 import 'package:flutter/services.dart';
 //신근재
 
-class Kakao extends StatelessWidget {
-  const Kakao({ Key? key }) : super(key: key);
+//<-------------------로그인 버튼 클래스 정의----------------------->
+class login_nextpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(
-          children: [
-            SizedBox(height: 200),
-            Image.asset("assets/mainpage_image.png", height: 300,width: 300,),
+    return ElevatedButton.icon(
+        icon: Icon(Icons.lock),
+        label: Text("카카오 로그인",style: TextStyle(fontSize: 17, color: Colors.black87),),
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.yellow),
+            foregroundColor: MaterialStateProperty.all(Colors.black54),
+            minimumSize: MaterialStateProperty.all(Size(100, 50))
+        ),
 
-            login_nextpage(),
-            SizedBox(height: 15),
-            login_logout(),
-            SizedBox(height: 15),
-            login_share(),
-          ],
-        )
+        onPressed: () async {
+          //[1] 카카오톡 설치 여부
+          if(await isKakaoTalkInstalled()){
+            try {
+              //[2] 이미 로그인 했나 토큰 유효성 확인 후 로그인 시도
+              AccessTokenInfo tokenInfo = await UserApi.instance.accessTokenInfo();
+              User user = await UserApi.instance.me();//유저 정보 user에 담는다.
+              print('토큰 정보 보기 성공'
+                  '\n회원정보: ${tokenInfo.id}'
+                  '\n토큰 만료시간: ${tokenInfo.expiresIn} 초');
+              //[3]정상적으로 토큰 성공을 한 경우 메인 페이지로 다시 돌아갑니다.
+              Navigator.push
+                (context,
+                MaterialPageRoute(builder: (context) => secondpage(user)),);
+            } catch (error) {
+              print('토큰 정보 보기 실패 $error');
+              try {
+                //[2-1] 카카오톡 로그인 접속 시도
+                await UserApi.instance.loginWithKakaoTalk();
+                User user = await UserApi.instance.me();
+                print('카카오톡으로 로그인 성공');
+                //★★★★★★★★★다음 페이지 넘어가면서 user넘겨줌★★★★★★★★★
+                //model폴더의 temp.dart를 보면 user 사용 예시 찾기 가능
+                Navigator.push
+                  (context,
+                  MaterialPageRoute(builder: (context) => secondpage(user)),);
+              } catch (error) {
+                print('카카오톡으로 로그인 실패 $error');
+              }
+            }
+          }
+          //[1-1 카카오톡 미설치
+          else{
+            showDialog(
+                context: context,
+                builder: (BuildContext context){
+                  return AlertDialog(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                    ),
+
+                    title: new Text("카카오톡 설치 후 실행해주세요!"),
+
+                    actions: <Widget>[
+                      new ElevatedButton(
+                        child: new Text("Close"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  );
+                }
+            );
+          }
+        }
     );
   }
 }
-
 //<-------------------로그인 버튼 클래스 정의----------------------->
 class login_nextpage extends StatelessWidget {
   @override
@@ -135,3 +186,25 @@ class login_share extends StatelessWidget {
     );
   }
 }
+
+//<----------------테스트용 코드 <삭제 가능>------------------>
+//class Kakao extends StatelessWidget {
+//   const Kakao({ Key? key }) : super(key: key);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Column(
+//           children: [
+//             SizedBox(height: 200),
+//             Image.asset("assets/mainpage_image.png", height: 300,width: 300,),
+//
+//             login_nextpage(),
+//             SizedBox(height: 15),
+//             login_logout(),
+//             SizedBox(height: 15),
+//             login_share(),
+//           ],
+//         )
+//     );
+//   }
+// }
