b187121b4a1e5d9c5bd9a182cb3c8e24caaaab1b
신근재 <shin75492@naver.com>
2022-12-04 23:40:33 +0900

    좌측 설정바에 사용자 정보 삽입
    221204

--- ootd/lib/screen/mainScreen.dart
+++ ootd/lib/screen/mainScreen.dart
@@ -8,54 +8,17 @@ import 'package:flutter_animated_icons/useanimations.dart';
 import 'package:lottie/lottie.dart';
 import 'package:lottie/lottie.dart';
 import 'package:flutter/services.dart';
+import 'package:ootd/model/model.dart';
 import 'package:ootd/screen/loading.dart';
 import 'package:ootd/screen/loading2.dart';
 import 'package:ootd/screen/settingScreen.dart';
 import 'package:ootd/screen/weather_screen.dart';
-import 'package:ootd/model/model.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ootd/API/kakao.dart';
 import 'package:ootd/screen/weekootdScreen.dart';
 import 'package:ootd/screen/alarm.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:ootd/screen/startScreen.dart';
-
-//신근재 카톡 로그인 <전역 변수,함수>
-
-void KakaoToken(){
-  Future<bool?> getT() async {
-    if (await AuthApi.instance.hasToken()) {
-      KakaoData.Token = true;
-      print('----------------------------------');
-      print('토큰 존재');
-      print('----------------------------------');
-
-      var user = await UserApi.instance.me();//유저 정보 user에 담는다.
-
-      KakaoData.userImage_URL = (user.kakaoAccount?.profile?.profileImageUrl).toString();
-      KakaoData.user_gen = (user.kakaoAccount?.gender).toString();
-      KakaoData.user_name = (user.kakaoAccount?.profile?.nickname).toString();
-      KakaoData.user_email = (user.kakaoAccount?.email).toString();
-
-      if(KakaoData.user_gen == "Gender.male"){
-        KakaoData.user_gen = "남성";
-      } else {
-        KakaoData.user_gen = "여성";
-      }
-      await Future.delayed(Duration(seconds: 1));//땡겨오는 시간 딜레이 대기
-      return true;
-    }
-    else {
-      KakaoData.Token = false;
-      print('----------------------------------');
-      print('토큰이 없어요. . .');
-      print('----------------------------------');
-      return false;
-    }
-  }
-
-  Future<bool?> future = getT();
-}
 
 class HomePageWidget extends StatefulWidget {
   // const HomePageWidget({Key? key}) : super(key: key);
@@ -70,6 +33,7 @@ class HomePageWidget extends StatefulWidget {
 
 class _HomePageWidgetState extends State<HomePageWidget>with TickerProviderStateMixin{
   Model model = Model();
+  KakaoData kakaodata = KakaoData();
   Widget ?icon;
   List <Widget> icons= [];
   String weather = 'weather';
@@ -192,17 +156,18 @@ class _HomePageWidgetState extends State<HomePageWidget>with TickerProviderState
               children: <Widget>[
                 UserAccountsDrawerHeader(
                   currentAccountPicture: CircleAvatar(
-                    backgroundColor: Colors.white,
+                    backgroundImage: KakaoData.Token? NetworkImage('${KakaoData.userImage_URL}') : NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7s_NWoe9O4ONIdRkRBZjASnTMGvQC5ajjtw&usqp=CAU')
                   ),
-                  accountName: Text('섹잘알 최지철',
+                  accountName: KakaoData.Token? Text('${KakaoData.user_name}',
                   style: TextStyle(
                     color: DarkMode.DarkOn? Colors.black :Colors.white,
                   ),
-                    ), accountEmail: Text('SexChoi@gmail.com',
+                    ) : Text('이름:로그인 후 나타납니다.'),
+                  accountEmail: KakaoData.Token? Text('${KakaoData.user_email}',
                   style: TextStyle(
                     color: DarkMode.DarkOn? Colors.black :Colors.white,
-                  ),
-                ),
+                  )
+                ) : Text('이메일:로그인 후 나타납니다.'),
                   decoration: BoxDecoration(
                       color: DarkMode.DarkOn? Colors.pink[400] :Colors.blue[300],
                       borderRadius: BorderRadius.only(
@@ -568,82 +533,14 @@ class _HomePageWidgetState extends State<HomePageWidget>with TickerProviderState
 
                       ],
                     )
-
-                        :
                     //<---------------------로그인 필요(토큰 없음)------------------------>
+                        :
                     Column(
                       children: [
                         Flexible(flex: 1, fit: FlexFit.tight, child: Container()),
 
-                        Flexible(flex: 1, fit: FlexFit.tight, child:
-                        ElevatedButton.icon(
-                            icon: Icon(Icons.lock),
-                            label: Text("카카오 로그인",style: TextStyle(fontSize: 18, color: Colors.black87),),
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(Colors.yellow),
-                                foregroundColor: MaterialStateProperty.all(Colors.black54),
-                                padding: MaterialStateProperty.all(EdgeInsets.all(20.0))
-                            ),
-                            onPressed: () async {
-                              //[1] 카카오톡 설치 여부
-                              if(await isKakaoTalkInstalled()){
-                                try {
-                                  //[2] 이미 로그인 했나 토큰 유효성 확인 후 로그인 시도
-                                  AccessTokenInfo tokenInfo = await UserApi.instance.accessTokenInfo();
-                                  User user = await UserApi.instance.me();//유저 정보 user에 담는다.
-                                  print('토큰 정보 보기 성공'
-                                      '\n회원정보: ${tokenInfo.id}'
-                                      '\n토큰 만료시간: ${tokenInfo.expiresIn} 초');
-                                  //[3]정상적으로 토큰 성공을 한 경우 메인 페이지로 다시 돌아갑니다.
-                                                                KakaoData.Token = true;
-                                  Navigator.push
-                                  (context,
-                                  MaterialPageRoute(builder: (context) => firstPage()));
-                                } catch (error) {
-                                  print('토큰 정보 보기 실패 $error');
-                                  try {
-                                    //[2-1] 카카오톡 로그인 접속 시도
-                                    await UserApi.instance.loginWithKakaoTalk();
-                                    User user = await UserApi.instance.me();
-                                    print('카카오톡으로 로그인 성공');
-                                    //★★★★★★★★★다음 페이지 넘어가면서 user넘겨줌★★★★★★★★★
-                                    //model폴더의 temp.dart를 보면 user 사용 예시 찾기 가능
-                                    KakaoData.Token = true;
-                                    Navigator.push
-                                      (context,
-                                      MaterialPageRoute(builder: (context) => firstPage()),);
-                                  } catch (error) {
-                                    print('카카오톡으로 로그인 실패 $error');
-                                  }
-                                }
-                              }
-                              //[1-1 카카오톡 미설치
-                              else{
-                                showDialog(
-                                    context: context,
-                                    builder: (BuildContext context){
-                                      return AlertDialog(
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.circular(10.0)
-                                        ),
+                        Flexible(flex: 1, fit: FlexFit.tight, child: KakaoLogin()),
 
-                                        title: new Text("카카오톡 설치 후 실행해주세요!"),
-
-                                        actions: <Widget>[
-                                          new ElevatedButton(
-                                            child: new Text("Close"),
-                                            onPressed: () {
-                                              Navigator.pop(context);
-                                            },
-                                          ),
-                                        ],
-                                      );
-                                    }
-                                );
-                              }
-                            }
-                          )
-                        ),
                         Flexible(flex: 1, fit: FlexFit.tight, child: Container()),
                       ],
                     )
